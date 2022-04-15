f5706d8138ab967f382232dfb2d48ea52356d059
rokku <70775740+rokkuu@users.noreply.github.com>
2022-04-15 17:18:33 +0200

    Initial commit

---
+++ src/main/java/com/github/rokkuu/warps/Warps.java
@@ -0,0 +1,17 @@
+package com.github.rokkuu.warps;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Warps extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
